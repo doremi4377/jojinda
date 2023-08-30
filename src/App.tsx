5d234a7d4e4f9d4a3ccc5d38ad950472e6a15573
do remi <doremi4377@gmail.com>
2023-08-31 02:19:56 +0900

    playlist 과제 최초구현

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import PlayList from "./PlayList/PlayList";
+
+function App() {
+  return (
+    <div className="App">
+      <PlayList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PlayList/PlayList.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+import "./PlayList.scss";
+
+function PlayList() {
+  const [names, setNames] = useState<{ id: number; title: string; link: string }[]>([]);
+  const [inputText, setInputText] = useState({
+    title: "",
+    link: "",
+  });
+  const [nextId, setNextId] = useState(0);
+
+  const { title, link } = inputText;
+
+  const onChange = (e: any) => {
+    const nextForm = {
+      ...inputText,
+      [e.target.name]: e.target.value,
+    };
+    setInputText(nextForm);
+  };
+
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      ...inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText({
+      title: "",
+      link: "",
+    });
+  };
+
+  const onRemove = (id: number) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+  return (
+    <div className="test">
+      PlayList
+      <div>
+        {names.map(({ id, title, link }) => (
+          <div key={id}>
+            <div key={id}>
+              <div>
+                {title} / {link}
+                <button type="button" onClick={() => onRemove(id)}></button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div>
+        <div>
+          <label>노래입력</label>
+          <input type="text" name="title" value={title} onChange={onChange} />
+        </div>
+        <div>
+          <label>유투브 링크 입력</label>
+          <input type="text" name="link" value={link} onChange={onChange} />
+        </div>
+        <button type="button" onClick={onClick}>
+          추가
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default PlayList;
